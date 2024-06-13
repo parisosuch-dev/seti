fb58e19dfa213dd210ec29c091208bbc20b0484e
parisosuch-dev <parisosuch.dev@gmail.com>
2024-06-12 20:10:50 -0700

    Fix starfield position

--- app/page.tsx
+++ app/page.tsx
@@ -3,14 +3,14 @@ import Starfield from "@/components/star-field";
 export default function Home() {
   return (
     <div className="h-full">
-      <div className="h-full bg-obsidian flex flex-col items-center justify-center">
+      <div className="h-full bg-obsidian flex flex-col items-center justify-center relative">
         <Starfield
-          starCount={2000}
+          starCount={4000}
           starColor={[255, 255, 255]}
           speedFactor={0.05}
           backgroundColor="black"
         />
-        <h1 className="text-white text-9xl text-center font-poppins font-black">
+        <h1 className="text-white text-9xl text-center font-poppins font-black absolute">
           DO THE STARS GAZE BACK?
         </h1>
       </div>
--- components/star-field.tsx
+++ components/star-field.tsx
@@ -145,14 +145,8 @@ export default function Starfield(props: Props) {
     <canvas
       id="starfield"
       style={{
-        padding: 0,
-        margin: 0,
-        position: "fixed",
-        top: 0,
-        right: 0,
-        bottom: 0,
-        left: 0,
-        zIndex: 10,
+        width: "100%",
+        height: "100%",
         opacity: 1,
         pointerEvents: "none",
         mixBlendMode: "screen",
