059beb02116567371ccda23bbe2f02ec70c9ba33
parisosuch-dev <parisosuch.dev@gmail.com>
2024-06-12 20:32:10 -0700

    overlay menu

--- components/nav-bar.tsx
+++ components/nav-bar.tsx
@@ -24,7 +24,7 @@ export default function NavBar() {
   ];
 
   return (
-    <div className="bg-obsidian">
+    <div className="bg-obsidian fixed top-0 left-0 z-10 w-full">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16">
           <div className="flex items-center">
@@ -84,7 +84,7 @@ export default function NavBar() {
         leaveTo="opacity-0 scale-95"
       >
         <div className="md:hidden" id="mobile-menu">
-          <div className="flex flex-col pt-8 items-center min-h-screen space-y-4">
+          <div className="flex flex-col pt-8 items-center min-h-screen space-y-4 bg-obsidian">
             {navLinks.map((link) => (
               <Link
                 className="text-white text-2xl font-poppins font-medium"
